8ce73c20570db6b4d7b5341e3ce26c3014241080
Mike Wooten <mwooten@fortinet.com>
2019-04-29 10:48:05 -0500

    update the apigateway integration

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -30,21 +30,12 @@ resource "aws_api_gateway_method" "proxy_root" {
 }
 
 resource "aws_api_gateway_deployment" "deploy" {
-  depends_on              = [ "aws_api_gateway_method.proxy_root" ]
+  depends_on              = [ "aws_api_gateway_integration.lambda" ]
 
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
   stage_name              = "${var.environment}"
 }
 
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
-  http_method             = "${aws_api_gateway_method.proxy_root.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${var.lambda_invoke_arn}"
-}
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
   resource_id             = "${aws_api_gateway_resource.resource.id}"
