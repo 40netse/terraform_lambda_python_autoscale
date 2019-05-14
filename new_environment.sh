6d47c4a0711c4d5cc611153cf55b5b700c2a8c13
Mike Wooten <mwooten@fortinet.com>
2019-05-14 16:05:49 -0500

    fixes to environment script

--- new_environment.sh
+++ new_environment.sh
@@ -2,13 +2,12 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install awscli -y
+tput clear
+echo
+echo "Provide AWS Credentials via "aws configure" and deployment specific parameters"
+echo
+echo
 aws configure
-sudo apt-get install python3 python3-pip python3-venv -y
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip3 install -r requirements.txt
-sudo snap install terraform
 #
 # Ask the user which region to deploy to
 #
@@ -18,17 +17,6 @@ if [[ -n "$userInput" ]]
 then
     region=$userInput
 fi
-#
-# replace everything between the two quotes on the line that starts with aws_region with the region user input
-#
-sed -i '/^aws_region/ s/"[^"][^"]*"/"'$region'"/' terraform.tfvars
-sed -i '/^availability_zone1/ s/"[^"][^"]*"/"'$region'-1a"/' terraform.tfvars
-sed -i '/^availability_zone2/ s/"[^"][^"]*"/"'$region'-1b"/' terraform.tfvars
-#
-# replace everything between the second set of two quotes on the line that contains with "aws_region":
-# with the region user input
-#
-sed -i '/"aws_region":/ s/"[^"][^"]*"/"'$region'"/2' autoscale/zappa_settings.json
 
 #
 # Ask the user for an s3 bucket name that doesn't exist
@@ -39,7 +27,6 @@ if [[ -n "$userInput" ]]
 then
     s3_bucket=$userInput
 fi
-sed -i '/"s3_bucket":/ s/"[^"][^"]*"/"'$s3_bucket'"/2' autoscale/zappa_settings.json
 
 #
 # Ask the user for a keypair that exists in the region
@@ -50,6 +37,39 @@ if [[ -n "$userInput" ]]
 then
     keypair=$userInput
 fi
+tput clear
+sudo apt-get install python3 python3-pip python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip3 install -r requirements.txt
+sudo snap install terraform
+
+#
+# Now modify the approriate files based on user input
+#
+# replace everything between the two quotes on the line that starts with aws_region with the region user input
+# then build the availability zones using the region... its assumed that it will be 1a and 1b
+#
+sed -i '/^aws_region/ s/"[^"][^"]*"/"'$region'"/' terraform.tfvars
+sed -i '/^availability_zone1/ s/"[^"][^"]*"/"'$region'a"/' terraform.tfvars
+sed -i '/^availability_zone2/ s/"[^"][^"]*"/"'$region'b"/' terraform.tfvars
+#
+# replace everything between the second set of two quotes on the line that contains with "aws_region":
+# with the region user input for the autoscale/zappa_settings.json file
+#
+sed -i '/"aws_region":/ s/"[^"][^"]*"/"'$region'"/2' autoscale/zappa_settings.json
+#
+# replace everything between the second set of two quotes on the line that contains with "s3_bucket":
+# with the s3_bucket user input for the autoscale/zappa_settings.json file
+#
+sed -i '/"s3_bucket":/ s/"[^"][^"]*"/"'$s3_bucket'"/2' autoscale/zappa_settings.json
+#
+# replace everything between the set of two quotes on the line that contains with "keypair":
+# with the keypair user input for the terraform.tfvars file
+#
 sed -i '/^keypair/ s/"[^"][^"]*"/"'$keypair'"/' terraform.tfvars
 
+tput clear
 echo "Verify the terraform.tfvars and autoscale/zappa_settings.json file for correct parameters"
+tput clear
